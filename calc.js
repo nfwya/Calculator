3498c45cbf6ee805f478f17e0265f56792add348
Doc_husky <nfwya0811@gmail.com>
2022-02-01 15:17:11 +0800

    remaining -> backspace function

---
+++ calc.js
@@ -0,0 +1,76 @@
+const allButtons = document.querySelectorAll("button");
+let bufferNumber;
+let newDisplay = false;
+const reduceNumber = function(str){
+    return str.replace(/^0*/,'');
+};
+const evaluate = function(ope, buffer, curr){
+   switch(ope){
+        case '+':
+            return (+buffer + +curr).toFixed(2);
+        case '-':
+            return (+buffer - +curr).toFixed(2);
+        case 'x':
+            return (+buffer * +curr).toFixed(2);
+        case '/':
+            if(curr == 0){
+                alert("Can not divide by 0!");
+                return 'ERROR';
+            }
+            else return (+buffer / +curr).toFixed(2);
+
+   } 
+}
+const reset = function(){
+    bufferNumber = undefined;
+    document.querySelector('.current-operator').textContent = '';
+    document.querySelector('.current-number').textContent = '';
+    document.querySelector('.current-number').classList.remove('has-decimal');
+    newDisplay = false;
+}
+
+const clickHandler = function(){
+    const currentNumber = document.querySelector('.current-number').textContent;
+    const operator = document.querySelector('.current-operator');
+    const display = document.querySelector('.current-number');
+    if(this.classList.contains('number')){
+        const newDigit = this.textContent;
+        if(newDisplay === false){
+            const newNumber = currentNumber + newDigit;
+            display.textContent = reduceNumber(newNumber);
+        }
+        else{
+           newDisplay = false;
+           display.textContent = newDigit;
+           document.querySelector('.current-number').classList.remove('has-decimal');
+        }
+        
+    }
+    else if(this.classList.contains('decimal')){
+        if(!display.classList.contains('has-decimal')){
+           display.classList.toggle('has-decimal');
+           display.textContent = currentNumber + '.'; 
+        }
+    }
+    else if(this.classList.contains('operator')){
+       if(bufferNumber === undefined){
+           bufferNumber = currentNumber;
+           newDisplay = true;
+       }
+       else if(newDisplay === false){
+           bufferNumber = evaluate(operator.textContent , bufferNumber, currentNumber)
+           newDisplay = true;
+       }
+       operator.textContent = this.textContent;
+    }
+    else if(this.classList.contains('equal')){
+        const newNumber = evaluate(operator.textContent, bufferNumber, currentNumber);
+        display.textContent = newNumber;
+        bufferNumber = undefined;
+        operator.textContent = '';
+    }
+    else if(this.classList.contains('ac')){
+        reset();
+    }
+};
+allButtons.forEach( button => button.addEventListener('click', clickHandler) )
